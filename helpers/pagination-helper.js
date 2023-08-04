fc1744d56df6695dc0d11eb4e5a0c654b004b42f
Lanways <mcbluedd@gmail.com>
2023-08-04 16:02:35 +0800

    feat: add pagination helper

---
+++ helpers/pagination-helper.js
@@ -0,0 +1,27 @@
+const getOffset = (limit = 10, page = 1) => (page - 1) * limit
+
+const getPagination = (limit = 10, page = 1, total = 50) => {
+  const totalPage = Math.ceil(total / limit)
+  const pages = Array.from({ length: totalPage }, (_, index) => index + 1)
+  let currentPage
+  if (page < 1) {
+    currentPage = 1
+  } else if (page > totalPage) {
+    currentPage = totalPage
+  } else {
+    currentPage = page
+  }
+  const prev = currentPage - 1 < 1 ? 1 : currentPage - 1
+  const next = currentPage + 1 > totalPage ? totalPage : currentPage + 1
+  return {
+    pages,
+    totalPage,
+    currentPage,
+    prev,
+    next
+  }
+}
+module.exports = {
+  getOffset,
+  getPagination
+}
