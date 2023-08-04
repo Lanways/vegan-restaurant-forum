2dbfc0ea5909b858ac67266898bdc33a18133d56
Lanways <mcbluedd@gmail.com>
2023-08-04 16:02:06 +0800

    feat: add auth middleware

---
+++ middleware/auth.js
@@ -0,0 +1,20 @@
+const helpers = require('../helpers/auth-helpers')
+
+const authenticated = (req, res, next) => {
+  if (helpers.ensureAuthenticated(req)) {
+    return next()
+  }
+  res.redirect('/signin')
+}
+const authenticatedAdmin = (req, res, next) => {
+  if (helpers.ensureAuthenticated(req)) {
+    if (helpers.getUser(req).isAdmin) return next()
+    res.redirect('/')
+  } else {
+    res.redirect('/signin')
+  }
+}
+module.exports = {
+  authenticated,
+  authenticatedAdmin
+}
