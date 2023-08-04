8500a58512ad1ceac4a8f3d0bd298510a99c9585
Lanways <mcbluedd@gmail.com>
2023-08-04 15:35:46 +0800

    feat: add signin route

--- contorllers/user-controller.js
+++ contorllers/user-controller.js
@@ -14,6 +14,10 @@ const userController = {
   signInPage: (req, res) => {
     res.render('signin')
   },
+  signIn: (req, res) => {
+    req.flash('success_messages', '成功登入!')
+    res.redirect('/restaurants')
+  },
 }
 
 module.exports = userController
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const router = express.Router()
 const userController = require('../contorllers/user-controller')
+const passport = require('../config/passport')
 
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
 router.get('/signin', userController.signInPage)
+router.post('/signin', passport.authenticate('local',
+  {
+    failureRedirect: '/signin',
+    failureFlash: true
+  }), userController.signIn)
+router.get('/restaurants', authenticated, restController.getRestaurants)
 
 module.exports = router
