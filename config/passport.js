fc72c195a12cb14fe95e5fcbb801add75c80e06a
Lanways <mcbluedd@gmail.com>
2023-08-16 00:43:38 +0800

    feat: add facebook strategy

--- config/passport.js
+++ config/passport.js
@@ -2,6 +2,7 @@ const passport = require('passport')
 const LocalStrategy = require('passport-local')
 const bcrypt = require('bcryptjs')
 const { User, Restaurant } = require('../models')
+const FacebookStrategy = require('passport-facebook').Strategy
 
 passport.use(new LocalStrategy(
   {
@@ -12,14 +13,44 @@ passport.use(new LocalStrategy(
   (req, email, password, cb) => {
     User.findOne({ where: { email } })
       .then(user => {
-        if (!user) return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤！'))
+        if (!user) {
+          req.session.input = req.body
+          return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤！'))
+        }
         bcrypt.compare(password, user.password).then(res => {
-          if (!res) return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤！'))
+          if (!res) {
+            req.session.input = req.body
+            return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤！'))
+          }
           return cb(null, user)
         })
       })
   }
 ))
+
+passport.use(new FacebookStrategy({
+  clientID: process.env.FACEBOOK_APP_ID,
+  clientSecret: process.env.FACEBOOK_APP_SECRET,
+  callbackURL: 'http://localhost:3000/auth/facebook/callback',
+  profileFields: ['email', 'displayName']
+}, (accessToken, refreshToken, profile, done) => {
+  const { name, email } = profile._json
+  User.findOne({ where: { email } })
+    .then(user => {
+      if (user) return done(null, user)
+      const randomPassword = Math.random().toString(36).slice(-8)
+      bcrypt
+        .genSalt(10)
+        .then(salt => bcrypt.hash(randomPassword, salt))
+        .then(hash => User.create({
+          name,
+          email,
+          password: hash
+        }))
+        .then(user => done(null, user))
+        .catch(err => done(err, false))
+    })
+}))
 
 passport.serializeUser((user, cb) => {
   cb(null, user.id)
