14c3590be11271b5be033d94de3b193e29001458
Lanways <mcbluedd@gmail.com>
2023-08-15 14:04:03 +0800

    feat: add redis for session storage

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ if (process.env.NODE_ENV !== 'production') {
 }
 const express = require('express')
 const app = express()
-const port = 3000 || process.env.PORT
+const port = process.env.PORT || 3000
 const routes = require('./routes')
 const handlebars = require('express-handlebars')
+const redis = require("redis")
 const session = require('express-session')
 const flash = require('connect-flash')
 const { getUser } = require('./helpers/auth-helpers')
@@ -14,14 +15,43 @@ const handlebarsHelpers = require('./helpers/handlebars-helpers')
 const path = require('path')
 const methodOverride = require('method-override')
 
+const RedisStore = require("connect-redis")(session)
+
+let client = redis.createClient({
+  host: process.env.REDIS_HOST || 'localhost',
+  port: 6379
+})
+
+client.on('ready', () => {
+  console.log('Redis client is ready.')
+})
+
+client.on('connect', () => {
+  console.log('Redis client has connected to the server.')
+})
+
+client.on('error', (err) => {
+  console.error('Redis error:', err)
+})
+
+client.on('end', () => {
+  console.error('Redis connection closed.')
+})
+
+
 app.engine('hbs', handlebars({ extname: '.hbs', helpers: handlebarsHelpers }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use('/upload', express.static(path.join(__dirname, 'upload')))
 app.use(methodOverride('_method'))
+app.use(session({
+  store: new RedisStore({ client }),
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}))
 
-app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false }))
 app.use(passport.initialize())
 app.use(passport.session())
 app.use(flash())
@@ -32,10 +62,10 @@ app.use((req, res, next) => {
   next()
 })
 
+app.use(routes)
+
 app.listen(port, () => {
   console.log(`express server is running on locahost:${port}`)
 })
-
-app.use(routes)
 
 module.exports = app
