b00575b15e96a99ddf6f1d9577426fe984e8941f
Lanways <mcbluedd@gmail.com>
2024-02-29 16:43:32 +0800

    bugfix: dbconfig use env

--- config/config.js
+++ config/config.js
@@ -1,9 +1,12 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 module.exports = {
   "development": {
-    "username": "root",
-    "password": "password",
-    "database": "vegan_restaurant_forum",
-    "host": "127.0.0.1",
+    "username": process.env.MYSQL_USER,
+    "password": process.env.MYSQL_PASSWORD,
+    "database": process.env.MYSQL_DATABASE,
+    "host": process.env.MYSQL_HOST,
     "dialect": "mysql"
   },
   "test": {
--- config/passport.js
+++ config/passport.js
@@ -2,7 +2,6 @@ const passport = require('passport')
 const LocalStrategy = require('passport-local')
 const bcrypt = require('bcryptjs')
 const { User, Restaurant } = require('../models')
-const FacebookStrategy = require('passport-facebook').Strategy
 const GoogleStrategy = require('passport-google-oauth20').Strategy
 
 passport.use(new LocalStrategy(
@@ -54,30 +53,6 @@ passport.use(new GoogleStrategy({
       })
   }
 ))
-
-passport.use(new FacebookStrategy({
-  clientID: process.env.FACEBOOK_APP_ID,
-  clientSecret: process.env.FACEBOOK_APP_SECRET,
-  callbackURL: process.env.FACEBOOK_CALLBACK_URL,
-  profileFields: ['email', 'displayName']
-}, (accessToken, refreshToken, profile, done) => {
-  const { name, email } = profile._json
-  User.findOne({ where: { email } })
-    .then(user => {
-      if (user) return done(null, user)
-      const randomPassword = Math.random().toString(36).slice(-8)
-      bcrypt
-        .genSalt(10)
-        .then(salt => bcrypt.hash(randomPassword, salt))
-        .then(hash => User.create({
-          name,
-          email,
-          password: hash
-        }))
-        .then(user => done(null, user))
-        .catch(err => done(err, false))
-    })
-}))
 
 passport.serializeUser((user, cb) => {
   cb(null, user.id)
